d89dc8394a9d78f17b27fc013311c0be6287ae1b
devbully-lang <americanbullycomunity3@gmail.com>
2025-07-26 17:26:26 +0100

    yesss

--- .next/static/chunks/src_app_service_page_tsx_0d75932d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_service_page_tsx_199ea602._.js"
-],
-    source: "dynamic"
-});
