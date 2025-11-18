8aa9786e10dd92fc4edf2998efaa9847fae82120
devbully-lang <americanbullycomunity3@gmail.com>
2025-11-18 18:27:35 +0100

    Update puppyData.ts

--- src/lib/puppyData.ts
+++ src/lib/puppyData.ts
@@ -132,7 +132,7 @@ export const availablePups = [
       '/images/puppies/cookie-3.jpg',
     ],
     price: 1200,
-    status: 'Reserved',
+    status: 'Available',
     gender: 'Female',
     breedType: 'American Bully',
     registry: 'CKC',
